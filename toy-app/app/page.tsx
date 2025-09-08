9103874d05eb3802cd3d1632614319d442cb2676
Luca <maxmustermann@fantasy.furtz>
2025-09-08 11:51:59 +0200

    try out working with props in JSX

--- toy-app/app/page.tsx
+++ toy-app/app/page.tsx
@@ -1,20 +1,54 @@
 import Image from "next/image";
+import { ReactElement, JSXElementConstructor, ReactNode, ReactPortal } from "react";
 
 export default function Home() {
   return (
  <div>
   <Header/>
-  <h6>this is the HOME</h6>
+  <h6>inline fixed HTML</h6>
+  <AcceptsProps content="hello"/>
+  <DivwithContent content="world"/>
+  <DivwithContentFallback/>
+  <DivwithContentFallback content="Fallback not needed"/>
+
   <Footer/>
   </div>
 
   );
 }
 
+//Question: should props be declared as type any? Better solutions?
+//Especially be carefull when working with data from outside sources
+function AcceptsProps(props: any){
+  console.log(props);
+  let stringObject = props.toString();
+   return(
+    <>
+    <div>Trying out props:</div>
+    <div>string Object: {stringObject} </div>
+    <div>inlineFunction: {props.toString()} </div>
+    <div>Acces attributes: {props.content}</div>
+    </>
+  );
+}
+
+  function DivwithContent({content}: any){
+     return(
+     <div>directly derefered attribute: {content}</div>
+     );
+  }
+
+    function DivwithContentFallback({content}: any){
+     return(
+     <div>Fallback-safe: {content ? content : "Default if prop is null"}</div>
+     );
+  }
+
+
 function Header(){
 return(
     <header>
-        <h1>Develop. Preview. Ship.</h1>
+        <h1>I am the header.</h1>
   </header>
 );
 }
@@ -26,6 +60,6 @@ function Footer(){
       <h6>This is the Footer</h6>
     </footer>
     </>
-  )
+  );
 }
 
